15882e467355f4608d2d147f30092abc02a1735d
AlessandroMemmolo <Alessandroruben01@gmail.com>
2025-03-22 14:41:36 +0100

    Created gradient descend basic

--- GradientDescend.py
+++ GradientDescend.py
@@ -2,4 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def GradientDescend()
+def GradientDescend(fun,gradient,x0, tol, alpha, maxIter):
+    n = 0
+    xpath = []
+    xpath.append(x0)
+    gpath = []
+    gpath.append(gradient(x0))
+    fpath = []
+    fpath.append(fun(x0))
+    while np.norm(gradient(x0)) > tol and n < maxIter:
+        x0 = x0 - alpha*gradient(x0)
+        n = n+1
+        xpath.append(x0)
+        gpath.append(gradient(x0))
+        fpath.append(fun(x0))
+    result = pd.DataFrame(data = [xpath,gpath,fpath], columns = ['x values', 'gradient values', 'function values'])
+    return result
